d948ea48f97ce5e7dc275a11045691766e7932e6
astux7 <Asta.bevainyte@gmail.com>
2014-01-06 21:21:21 +0000

    interface

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,40 +1,46 @@
-//define class player
+function Pick(name) {
+  this.name = name;
+}
+
+Pick.prototype.pairs = {
+  scissors: { paper: 'cuts', lizard: 'decapitates'},
+  paper:    { rock: 'covers', spock: 'disproves'},
+  lizard:   { spock: 'poisons', paper: 'eats' },
+  rock:     { scissors: 'smashes', lizard: 'crushes' },
+  spock:    { rock: 'vaporizes', scissors: 'melts' }
+}
+
+Pick.prototype.beats = function(otherPick) {
+  var verb = this.pairs[this.name][otherPick.name];
+
+  if(verb) {
+    return [this.name, verb, otherPick.name].join(' ');
+  } else {
+    return false;
+  }
+}
+
+// Player class
 function Player(name) {
   this.name = name;
-  // console.log("New player created")
 }
-//define method picks
-Player.prototype.picks = function(pick) {
-	this.pick = pick;
-	//console.log("I am here");
+
+Player.prototype.picks = function(playerPick) {
+  this.pick = new Pick(playerPick);
 }
-//define class game
+
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
 }
 
-//define constant of class Game
-Game.prototype.PAIRS = {
-    'rock':     { 'beats': 'scissors' },
-    'paper':    { 'beats': 'rock' },
-    'scissors': { 'beats': 'paper' }
-}
-
-//define method winner
 Game.prototype.winner = function() {
-  if(this.samePick()){
-    return null
-  }
-  if (this.PAIRS[this.player1.pick]['beats'] === this.player2.pick) {
-    return this.player1
-  }
-  return this.player2
-}
-//define method samePick
-Game.prototype.samePick = function() {
-	if (this.player1.pick === this.player2.pick){
-		return true;
-	}
-	return false;
+  var p1Wins = this.player1.pick.beats(this.player2.pick);
+  var p2Wins = this.player2.pick.beats(this.player1.pick);
+
+  // console.log(p1Wins || p2Wins || 'draw');
+  if(p1Wins) return p1Wins;
+  if(p2Wins) return p2Wins;
+  
+  return 'Draw';
 }
